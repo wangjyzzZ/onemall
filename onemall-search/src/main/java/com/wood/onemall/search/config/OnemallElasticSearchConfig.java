24841dc4e4e8da1264b17b955835f9b7f067055a
wangjy <wangjianyang111@163.com>
2025-05-18 22:48:29 +0800

    search服务

---
+++ onemall-search/src/main/java/com/wood/onemall/search/config/OnemallElasticSearchConfig.java
@@ -0,0 +1,26 @@
+package com.wood.onemall.search.config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OnemallElasticSearchConfig {
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+        COMMON_OPTIONS = builder.build();
+    }
+
+    @Bean
+    public RestHighLevelClient esRestClient() {
+        return new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("192.168.56.101", 9200, "http")));
+    }
+}
---
+++ onemall-search/src/test/java/com/wood/onemall/search/OnemallSearchApplicationTests.java
@@ -0,0 +1,71 @@
+package com.wood.onemall.search;
+
+import com.alibaba.fastjson.JSON;
+import com.wood.onemall.search.config.OnemallElasticSearchConfig;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnemallSearchApplicationTests {
+
+    @Autowired
+    private RestHighLevelClient client;
+
+    @Test
+    public void searchData() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("bank");
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("address", "mill"));
+
+        searchRequest.source(searchSourceBuilder);
+
+        SearchResponse searchResponse = client.search(searchRequest, OnemallElasticSearchConfig.COMMON_OPTIONS);
+
+        System.out.println(searchResponse);
+    }
+
+    /**
+     * 测试存储数据
+     */
+    @Test
+    public void indexData() throws IOException {
+        IndexRequest indexRequest = new IndexRequest("users");
+        indexRequest.id("1");
+//        indexRequest.source("userName", "zhangsan", "age", 18, "gender", "男");
+        User user = new User();
+        user.setUserName("zhangsan");
+        user.setAge(18);
+        user.setGender("男");
+        String jsonString = JSON.toJSONString(user);
+        indexRequest.source(jsonString, XContentType.JSON);
+
+        IndexResponse index = client.index(indexRequest, OnemallElasticSearchConfig.COMMON_OPTIONS);
+
+        System.out.println(index);
+    }
+
+    @Data
+    class User {
+        private String userName;
+        private String gender;
+        private Integer age;
+    }
+
+}
